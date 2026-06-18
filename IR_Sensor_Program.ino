554a368963994ce8de5bc35c51553438510e765c
Chinmay-24 <naiks.chinmay@gmail.com>
2026-06-18 18:25:00 +0530

    Refactor IR sensor program for clarity

--- IR_Sensor_Program.ino
+++ IR_Sensor_Program.ino
@@ -1,27 +1,19 @@
-//Program to interface an Infra Red (IR) Sensor with Arduino
-
-//External LED should glow when an object come near the sensor
-
-//Connections : Connector 10 to Connector S1, Connector 12 to Connector D3
-
-const int irSensor = 2; //Define Pin for IR Sensor
-const int led_pin = 10; //Define Pin for LED
-
+const int irSensor = 2; 
+const int led_pin = 10; 
 void setup() {
-  pinMode(irSensor, INPUT); //Configure the Pin as INPUT Pin
-  pinMode(led_pin, OUTPUT); //Configure the Pin as OUTPUT Pin
-  Serial.begin(9600); //This is needed to see the sensor output in serial monitor
+  pinMode(irSensor, INPUT); 
+  pinMode(led_pin, OUTPUT); 
+  Serial.begin(9600); 
 }
-
 void loop() {
-  int value = digitalRead(irSensor); // Read the sensor value
+  int value = digitalRead(irSensor); 
   Serial.println(value);
   if (value == 0) {
-                      digitalWrite(led_pin, 0); // Turn on the LED if obstacle is detected
+                      digitalWrite(led_pin, 0); 
                   }
   else {
-          digitalWrite(led_pin, 1); // Turn off the LED if no obstacle is detected
+          digitalWrite(led_pin, 1); 
         }
   
-  delay(100); // Adjust this as needed
+  delay(100); 
 }
