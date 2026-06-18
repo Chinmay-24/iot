9a776942a1d2297a7a42cd4ffb612987b01a78c1
Chinmay-24 <naiks.chinmay@gmail.com>
2026-06-18 18:24:18 +0530

    Remove comments and clean up buzzer program

--- Buzzer_Program.ino
+++ Buzzer_Program.ino
@@ -1,15 +1,10 @@
-//Program to Interface Buzzer with Arduino
-
-//Connection : Connector 10 to Connector A3
-
-const int buzzer_pin = 2; //Define the pin to connect Buzzer
-
+const int buzzer_pin = 2; 
 void setup() {
-  pinMode(buzzer_pin, OUTPUT); //Configure the Pin as OUTPUT port
+  pinMode(buzzer_pin, OUTPUT); 
 }
 void loop() {
   digitalWrite(buzzer_pin, 1);
-  delay(1000);//Turn the Buzzer OFF for 1000 milli second
+  delay(1000);
   digitalWrite(buzzer_pin, 0);
-  delay(2000);//Turn the Buzzer ON for 1000 milli second
+  delay(2000);
 }
